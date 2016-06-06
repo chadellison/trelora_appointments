12ef73a477da21129e815e3a445884ae78b3cd2e
Chad Ellison <chad.ellison0123@gmail.com>
2016-06-05 19:50:17 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,39 @@
+source 'https://rubygems.org'
+ruby '2.3.0'
+gem 'geocoder'
+gem 'responders'
+gem 'active_model_serializers', github: 'rails-api/active_model_serializers'
+gem 'rails', '4.2.6'
+gem 'pg'
+gem 'sass-rails', '~> 5.0'
+gem 'figaro'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'unicorn'
+gem 'bcrypt', '~> 3.1.7'
+gem 'faraday'
+
+group :development, :test do
+  gem 'selenium-webdriver'
+  gem 'pry-rails'
+  gem 'rails_12factor'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'launchy'
+  gem 'capybara-webkit'
+  gem 'vcr'
+  gem 'webmock'
+  gem 'shoulda-matchers', github: 'thoughtbot/shoulda-matchers'
+  gem "database_cleaner"
+  gem 'mocha'
+  gem 'simplecov', :require => false
+  gem 'rspec-rails', '~> 3.0'
+end
+
+group :development do
+  gem 'web-console', '~> 2.0'
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,45 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require "capybara/rails"
+require 'vcr'
+require 'webmock'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = true # change to false if using selenium
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/casettes"
+  c.hook_into :webmock
+end
+
+config.before(:suite) do
+  DatabaseCdata
+  DatabaseCleaner.strategy = :transaction # rollleaner.clean_with :truncation  # clean DB of any leftover back transactions between each test
+end
+
+config.before(:each) do
+  DatabaseCleaner.start
+end
+
+config.after(:each) do
+  DatabaseCleaner.clean
+end
