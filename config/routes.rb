11c72a68b6607b26c4edfd9b512b55b43359453c
thomp <thompickett@gmail.com>
2016-06-11 14:50:16 -0600

    Add ability to logout
    * Add sessions destroy route and action
    * Make link to logout if current user is logged in
    * Hide link to login if there is current user
    * In from omniauth method require google plus profile to be on user in order for them to login
    * Add add method update_or_create for user and helper method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get "/auth/google_oauth2", as: :google_login
   get 'auth/:provider/callback', to: 'sessions#create'
+  # get '', to: 'sessions#create'
+  delete '/session', to: 'sessions#destroy', as: :session
 
   namespace "api", defaults: { :format => 'json' } do
     namespace "v1" do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610054355) do
+ActiveRecord::Schema.define(version: 20160611200129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20160610054355) do
     t.datetime "updated_at",  null: false
     t.string   "uid"
     t.string   "oauth_token"
+    t.string   "profile"
   end
 
   add_foreign_key "appointments", "locations"
