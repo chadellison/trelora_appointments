cda880e8529b38a5e1af37f0ef79ffd7f4641011
thomp <thompickett@gmail.com>
2016-06-15 01:59:36 -0600

    Add functionality to determine closest route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace "v1" do
       resources :users, only: [:show, :create, :destroy]
       resources :field_workers, only: [:index, :create, :update]
+      get "/best_appointment", to: "appointments#best_appointment"
     end
   end
 end
