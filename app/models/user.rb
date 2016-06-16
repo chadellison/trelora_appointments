ed325bae1a8ad3dcecd225b7594c5d65efc09b17
Chad Ellison <chad.ellison0123@gmail.com>
2016-06-16 05:36:24 -0600

    Remove unnecessary tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ActiveRecord::Base
-  has_many :user_roles
-  has_many :roles, through: :user_roles
-
   enum role: [ :default, :admin, :keith ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616111513) do
+ActiveRecord::Schema.define(version: 20160616112609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,22 +47,6 @@ ActiveRecord::Schema.define(version: 20160616111513) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "user_roles", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "role_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "user_roles", ["role_id"], name: "index_user_roles_on_role_id", using: :btree
-  add_index "user_roles", ["user_id"], name: "index_user_roles_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.datetime "created_at",             null: false
@@ -72,6 +56,4 @@ ActiveRecord::Schema.define(version: 20160616111513) do
   end
 
   add_foreign_key "appointments", "locations"
-  add_foreign_key "user_roles", "roles"
-  add_foreign_key "user_roles", "users"
 end
