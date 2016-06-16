bf8554b7a30e44375b342b248e71c15c4b757b0c
Chad Ellison <chad.ellison0123@gmail.com>
2016-06-16 05:24:10 -0600

    Add admin authorization functionality Add admin helper Add current_user helper

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613235121) do
+ActiveRecord::Schema.define(version: 20160616111513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,12 +65,10 @@ ActiveRecord::Schema.define(version: 20160613235121) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "uid"
-    t.string   "oauth_token"
-    t.string   "profile"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "password"
+    t.integer  "role",       default: 0
   end
 
   add_foreign_key "appointments", "locations"
