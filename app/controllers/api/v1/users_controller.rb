e9b4ecc248733a3e5417596ab3aafbb286024cca
Chad Ellison <chad.ellison0123@gmail.com>
2016-06-18 13:57:02 -0600

    Add flash message for create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,9 @@ class Api::V1::UsersController < Api::ApiController
   respond_to :json
 
   def create
-    user = User.create(user_params)
-    flash[:creat_user] = "#{user_params[:username]} was created"
-    respond_with user, location: nil
-    # redirect_to root_path
+    User.create(user_params)
+    flash[:success] = "#{user_params[:username]} was created"
+    redirect_to root_path
   end
 
   def show
@@ -19,6 +18,6 @@ class Api::V1::UsersController < Api::ApiController
   private
 
   def user_params
-    params.require(:user).permit(:username, :role, :token, :password, :id)
+    params.require(:user).permit(:username, :role, :password)
   end
 end
